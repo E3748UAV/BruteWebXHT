0732486646b76910ef6e682c4b6a8fac09ee142f
E3748UAV <45319785+E3748UAV@users.noreply.github.com>
2018-12-23 16:24:06 +0700

    Add files via upload

---
+++ BruteXHT.sh
@@ -0,0 +1,162 @@
+#usr/bin/bash
+clear
+echo "\033[37;1m"
+toilet -f standard -F gay "BruteWebdav"
+clear
+toilet -f standard -F gay "XHT"
+echo "\033[34;1m"
+echo "\033[32;1mauthor: E3748UAV"
+echo "\033[33;1mTeam: Xploiter Hacker Team"
+echo "\033[39;1mUntuk Join Team Bisa Contact saya"
+echo "\033[30;1mNothing Is Save"
+echo "\033[32;1mNo Name Cyber Team"
+echo ""
+echo ""
+echo "\033[32;1mCopyright © 2018-2019 Xploiter Hacker Team"
+echo "\033[32;1m[01].Brute Deface Webdav Exploiter Hackers Team"
+echo "\033[33;1m[02].EXIT"
+echo "\033[38;1m[03].Contact me"
+echo "\033[32;1mCopyright © 2018-2019 Xploiter Hacker Team"
+read -p "[========>" E3748UAV
+echo ""
+echo ""
+sleep 1
+if [ $E3748UAV = 01 ] || [ $E3748UAV = 1 ]
+then
+echo "Masukan Script !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white                               
+read -p " ╰─$ " script
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white                               
+read -p " ╰─$ " script
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+echo "Masukan Target !!!!"
+echo $white "╭─"$green"XHT@localhost"$cyan" ~/XHTsystem"$white
+read -p " ╰─$ " target
+
+echo "[=============Jangan Lupa Sapa Kami Jika Ketemu Di Groub=============]"
+echo "\033[32;1mCopyright © 2018-2019 Xploiter Hacker Team"
+fi
+if [ $E3748UAV = 02 ] || [ $E3748UAV = 2 ]
+then
+echo "\033[32;1mWe Are Family XPLOITER HACKER TEAM"
+sleep 1
+echo
+echo " "Jangan Anggap Kami Sebagai Penjahat Cyber! Anggap lah kami sebagai Scurity Cyber :) "
+exit
+fi
